ff08f16c9d7c238e5c599497dfbff7446c810f7f
Larysa Rud <larpavrud@gmail.com>
2024-04-26 22:53:48 -0400

    Part 3: Adding Menu Buttons

--- index.js
+++ index.js
@@ -31,4 +31,16 @@ topMenuEl.style.setProperty("background", "var(--top-menu-bg)");
 //Add a class of flex-around to topMenuEl.
 topMenuEl.classList.add("flex-around");
 
+//Part 3: Adding Menu Buttons
+/*Iterate over the entire menuLinks array and for each "link" object:
+  Create an <a> element.
+  On the new element, add an href attribute with its value set to the href property of the "link" object.
+  Set the new element's content to the value of the text property of the "link" object.
+  Append the new element to the topMenuEl element. */
 
+/*var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+]; */
